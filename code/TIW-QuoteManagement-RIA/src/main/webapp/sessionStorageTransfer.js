7746a7e1792abbe3ef7c2e8033df00b8837b6cbc
ManuMerlo <merlomanu1999@gmail.com>
2022-06-11 16:59:38 +0200

    Finished Ria documentation

--- code/TIW-QuoteManagement-RIA/src/main/webapp/sessionStorageTransfer.js
+++ code/TIW-QuoteManagement-RIA/src/main/webapp/sessionStorageTransfer.js
@@ -2,28 +2,30 @@
 
 	if (!sessionStorage.length) {
 		// Ask other tabs for session storage
+		// Se la sessione è vuota, chiedo la sessione ad altre tab 
 		localStorage.setItem('getSessionStorage', Date.now());
 	};
 
+	//we have a component that will listen to all storage events
 	window.addEventListener('storage', function(event) {
 
-		//console.log('storage event', event);
-
+	
 		if (event.key == 'getSessionStorage') {
 			// Some tab asked for the sessionStorage -> send it
-
+			// Appena accedo alla pagina nuova, invio un evento richiedendo i dati
+			// localStorage.setItem genera un altro evento, con key Session storage dunque la pagina che 
+			// ha richiesto la sessione, può leggerla tramite il localStorage.
 			localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
+			//subito dopo aver messo il valore, viene tolto per non rendereil salvataggio permanente alla chiusura del browser
 			localStorage.removeItem('sessionStorage');
 
 		} else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-			// sessionStorage is empty -> fill it
-
+			// viene preso il valore caricato nel localStorage
 			var data = JSON.parse(event.newValue);
 
 			for (key in data) {
 				sessionStorage.setItem(key, data[key]);
 			}
-
 		}
 	});
 
